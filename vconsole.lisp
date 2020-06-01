1b9f7082b83fd8bae89957a8aae2718b061f4def
Daniel Kochmański <daniel@turtleware.eu>
2020-06-01 19:08:41 +0200

    rethinking output: rework vconsole (1)
    - rename console -> console
    - remove :after methods
    - replace POS with ROW and COL
    - remove APP

--- vconsole.lisp
+++ vconsole.lisp
@@ -39,13 +39,12 @@
   "Put an object on a console"
   `(let ((str (princ-to-string ,object)))
      (assert (null (find #\newline str)))
-     (letf (((pos *console*) (cons (or ,row (car (pos *console*)))
-                                   (or ,col (cdr (pos *console*)))))
+     (letf (((row *console*) (or ,row (row *console*)))
+            ((col *console*) (or ,col (col *console*)))
             ,@(when fgc `(((fgc *console*) ,fgc)))
             ,@(when bgc `(((bgc *console*) ,bgc))))
-       (let* ((pos (pos *console*))
-              (row (car pos))
-              (col (cdr pos)))
+       (let ((row (row *console*))
+             (col (col *console*)))
          (loop for c from col
                for s across str
                if (inside row c)
@@ -61,10 +60,10 @@
                          (:clr `(clear-rectangle ,@args))
                          (:fgc `(setf (fgc *console*) (list ,@args)))
                          (:bgc `(setf (bgc *console*) (list ,@args)))
-                         (:cvp `(set-cursor-visibility ,@args))
-                         (:ptr `(set-mouse-tracking ,@args))
-                         (:pos `(setf (pos *console*) (cons ,(car args)
-                                                            ,(cdr args)))))))))
+                         (:cvp `(setf (cvp *console*) ,@args))
+                         (:ptr `(setf (ptr *console*) ,@args))
+                         (:row `(setf (row *console*) ,@args))
+                         (:col `(setf (col *console*) ,@args)))))))
 
 (defun clear-rectangle (r1 c1 r2 c2)
   (with-cursor-position (r1 c1)
@@ -89,29 +88,29 @@
       (setf *row2* (list rows))
       (setf *col2* (list cols)))))
 
-(defclass console ()
+(defclass vconsole ()
   ((ios :initarg :ios :accessor ios :documentation "Console I/O stream.")
    (fgc :initarg :fgc :accessor fgc :documentation "Foregorund color.")
    (bgc :initarg :bgc :accessor bgc :documentation "Background color.")
-   (pos :initarg :pos :accessor pos :documentation "Cursor position.")
    (cvp :initarg :cvp :accessor cvp :documentation "Cursor visibility.")
    (ptr :initarg :ptr :accessor ptr :documentation "Pointer tracking.")
    (fps :initarg :fps :accessor fps :documentation "Desired framerate.")
-   (app :initarg :app :accessor app :documentation "Application state.")
    (hnd               :accessor hnd :documentation "Terminal handler.")
+   (row :initarg :row :accessor row :documentation "Cursor row.")
+   (col :initarg :col :accessor col :documentation "Cursor col.")
    (rows :accessor rows             :documentation "Terminal number of rows.")
    (cols :accessor cols             :documentation "Terminal number of cols."))
   (:default-initargs :ios (error "I/O stream must be specified.")
                      :fgc '(#xff #xa0 #xa0)
                      :bgc '(#x22 #x22 #x22)
-                     :pos '(1 . 1)
+                     :row 1
+                     :col 1
                      :cvp nil
                      :ptr t
-                     :fps 10
-                     :app nil))
+                     :fps 10))
 
 (defmethod initialize-instance :after
-    ((instance console) &key fgc bgc pos cvp ptr)
+    ((instance vconsole) &key fgc bgc pos cvp ptr)
   (setf (hnd instance) (init-console))
   (apply #'set-foreground-color fgc)
   (apply #'set-background-color bgc)
@@ -121,30 +120,12 @@
   (let ((*console* instance))
     (update-console-dimensions)))
 
-(defmethod (setf fgc) :after (rgb (instance console))
-  (apply #'set-foreground-color rgb))
-
-(defmethod (setf bgc) :after (rgb (instance console))
-  (apply #'set-background-color rgb))
-
-(defmethod (setf pos) :before (pos (instance console))
-  (check-type (car pos) (integer 1))
-  (check-type (cdr pos) (integer 1)))
-
-(defmethod (setf pos) :after (pos (instance console))
-  (set-cursor-position (car pos) (cdr pos)))
-
-(defmethod (setf ptr) :after (ptr (instance console))
-  (set-mouse-tracking (not (null ptr))))
-
-(defmethod (setf cvp) :after (cvp (instance console))
-  (set-cursor-visibility (not (null cvp))))
 
 (defmacro with-console ((&rest args
                          &key ios fgc bgc cvp fps &allow-other-keys)
                         &body body)
   (declare (ignore fgc bgc cvp fps))
   `(let* ((*console-io* ,ios)
-          (*console* (make-instance 'console ,@args)))
+          (*console* (make-instance 'vconsole ,@args)))
      (unwind-protect (progn ,@body)
        (close-console (hnd *console*)))))
