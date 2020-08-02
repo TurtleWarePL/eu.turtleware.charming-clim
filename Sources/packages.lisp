3f957a9d88629046e044ba4df555663f267557e1
Daniel Kochmański <daniel@turtleware.eu>
2020-08-02 12:31:36 +0200

    packages: improve exports

--- Sources/packages.lisp
+++ Sources/packages.lisp
@@ -10,14 +10,15 @@
            #:clear-terminal
 
            #:clear-line
+           #:reset-text-style
            #:set-foreground-color
            #:set-background-color
+           #:set-text-style
 
            #:with-cursor-position
            #:set-cursor-position
            #:save-cursor-position
            #:restore-cursor-position
-           #:request-cursor-position
 
            #:cursor-up
            #:cursor-down
@@ -25,11 +26,27 @@
            #:cursor-left
 
            #:set-cursor-visibility
-           #:set-mouse-tracking))
+           #:set-mouse-tracking
+
+           #:*request-terminal-size*
+           #:request-cursor-position
+
+           #:event
+           #:terminal-event
+           #:unknown-terminal-event #:seq
+           #:cursor-position-event #:row #:col
+           #:terminal-resize-event #:rows #:cols
+           #:keyboard-event #:key #:kch #:mods
+           #:pointer-event #:row #:col #:btn #:mods #:state
+           #:pointer-motion-event
+           #:pointer-press-event
+           #:pointer-release-event))
 
 (defpackage #:eu.turtleware.charming-clim/l1
-  (:export #:with-console #:out #:ctl)
-  (:export #:console #:handle-event #:handle-repaint))
+  (:export #:*console* #:*buffer*)
+  (:export #:with-console #:with-buffer #:out #:ctl)
+  (:export #:process-available-events #:exit)
+  (:export #:console #:surface #:handle-event #:handle-repaint))
 
 (defpackage #:eu.turtleware.charming-clim/l2
   (:export))
