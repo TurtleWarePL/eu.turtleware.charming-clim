677460a943146abe8272e48258f851620100cf45
Daniel Kochmański <daniel@turtleware.eu>
2020-08-23 13:13:59 +0200

    fix typo

--- Examples/l1-wool.lisp
+++ Examples/l1-wool.lisp
@@ -14,7 +14,7 @@
 
 (defun draw-text (string row col &key (align :left))
   (flet ((do-it (row col)
-           (out (:row row :col col) string)))
+           (l1:out (:row row :col col) string)))
     (ecase align
       (:left   (do-it row col))
       (:center (do-it row (- col (truncate (length string) 2))))
