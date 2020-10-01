f5fd01334016cdf52df1f913e394fc1b99d3f63b
Daniel Kochmański <daniel@turtleware.eu>
2020-10-01 15:53:41 +0200

    l1-surfaces: all text buffer pre-example

--- Examples/l1-surfaces.lisp
+++ Examples/l1-surfaces.lisp
@@ -8,41 +8,12 @@
 ;;; - multiple moveable and resizable surfaces
 ;;; - events are redirected to a focused/pressed surface
 ;;; - multiple pointers may be used simultaneously
+;;; - example text buffer cursor implementation
 
-(defclass frame (l1:surface)
-  ())
-
-(defclass window (l1:surface)
-  ((title
-    :initarg :title
-    :accessor title)
-   ;; The hash table keys are pointers which are currently moving the window
-   ;; and its values are conses which indicate the pointer "last" position. We
-   ;; need the last coordinate to compute the motion difference.
-   (moving-pointers
-    :initform (make-hash-table :test #'eq)
-    :accessor moving-pointers)
-   ;; The hash table keys are pointers which currently resize the window. Its
-   ;; values are always T.
-   (resize-pointers
-    :initform (make-hash-table :test #'eq)
-    :accessor resize-pointers)
-   ;; Frame is the actual application.
-   (frame
-    :accessor frame)))
-
-(defmethod initialize-instance :after ((object window) &rest args
-                                       &key fgc bgc r1 c1 r2 c2)
-  (declare (ignore args))
-  ;; The frame mode is direct, because we are not interested in buffering
-  ;; output that will be buffered (again) in the window.
-  (let ((rows (+ (- r2 r1) 1))
-        (cols (+ (- c2 c1) 1)))
-   (setf (frame object)
-         (make-instance 'frame :sink object :mode :dir
-                               :fgc fgc :bgc bgc
-                               :r1 2 :c1 3
-                               :r2 (- rows 1) :c2 (- cols 2)))))
+;;; A function of the same name (and similar purpose) will be introduced for
+;;; display lists in the l2 module.
+(defgeneric handle-repaint (object)
+  (:method (object) nil))
 
 (defclass surface-manager (l1:console)
   ((surfaces
@@ -67,71 +38,19 @@
         (list (make-instance 'window
                              :title "Window 1"
                              :sink object
+                             :frame 'peep-frame
                              :fgc +purple+
                              :bgc +grey1+
                              :r1 2 :c1 4 :r2 10 :c2 21)
               (make-instance 'window
                              :title "Window 2"
                              :sink object
+                             :frame 'text-frame
                              :fgc +black+
                              :bgc +grey2+
-                             :r1 13 :c1 10 :r2 21 :c2 27))))
+                             :r1 13 :c1 10 :r2 21 :c2 40))))
 
-(defun draw-decorations (surface)
-  (multiple-value-bind (r1 c1 r2 c2) (l1:bbox surface)
-    (let ((rows (+ (- r2 r1) 1))
-          (cols (+ (- c2 c1) 1))
-          (border-fgc #x8800ff00)
-          (border-bgc #xffff8800))
-      (block nil
-        (ax:maphash-values (lambda (s)
-                             (when (eq surface s)
-                               (setf border-fgc #x8800ff00)
-                               (setf border-bgc #xff888800)
-                               (return)))
-                           (pressed l1:*console*)))
-      (let ((title (title surface)))
-        (l1:ctl (:ink border-fgc border-bgc)
-                (:clr 1 1 rows cols)
-                (:ink #x8800ff00 #x22222200)
-                (:clr 2 3 (- rows 1) (- cols 2))
-                (:ink border-fgc border-bgc))
-        (l1:out (:row 1 :col 1) title)
-        (when (eq surface (focused l1:*console*))
-          (l1:out (:row 1 :col (+ (length title) 1) :txt '(:blink t)) "█"))
-        (l1:out (:row rows :col cols) "/")))))
-
-;;; A function of the same name (and similar purpose) will be introduced for
-;;; display lists in the l2 module.
-(defgeneric handle-repaint (object)
-  (:method (object) nil))
-
-(defmethod handle-repaint ((object surface-manager))
-  (l1:ctl (:ink #xffffff00 #x00000000)
-          (:clr 1 1 (l0:rows object) (l0:cols object)))
-  (mapc (lambda (surface)
-          (handle-repaint surface)
-          #+ (or)
-          (multiple-value-bind (r1 c1 r2 c2) (l1:bbox surface)
-            (l1:out (:row r1 :col c1) "X")
-            (l1:out (:row r2 :col c2) "Y")))
-        (reverse (surfaces object)))
-  (l1:flush-output object))
-
-(defmethod handle-repaint ((object window))
-  (l1:with-buffer (object)
-    (draw-decorations object))
-  (handle-repaint (frame object))
-  ;; We need "force" here to reflush the background onto the sink that
-  ;; redraws its own background upon C-r. The more robust solution would be
-  ;; a protocol which allows signaling damaged regions to children. That
-  ;; will be implemented in the l2 module.
-  (l1:flush-output object :force t))
-
-(defmethod handle-repaint ((object frame))
-  (l1:with-buffer (object)
-    (draw-peep 1 1 nil)))
-
+;;; A helper function.
 (defun inside-p (surface row col)
   (multiple-value-bind (r1 c1 r2 c2)
       (l1:bbox surface)
@@ -148,6 +67,14 @@
            :move)
           (t
            t))))
+
+(defmethod handle-repaint ((object surface-manager))
+  (l1:ctl (:ink #xffffff00 #x00000000)
+          (:clr 1 1 (l0:rows object) (l0:cols object)))
+  (mapc (lambda (surface)
+          (handle-repaint surface))
+        (reverse (surfaces object)))
+  (l1:flush-output object))
 
 ;;; Our event handling is quite simplistic. The pointer events left button
 ;;; press and left button release are used to set the pressed window, and the
@@ -198,7 +125,75 @@
   (ax:when-let ((surface (focused object)))
     (l1:handle-event surface event)))
 
-(defmethod l1:handle-event ((object window) (event l0:keyboard-event)))
+
+(defclass window (l1:surface)
+  ((title
+    :initform (ax:required-argument :title)
+    :initarg :title
+    :accessor title)
+   ;; The hash table keys are pointers which are currently moving the window
+   ;; and its values are conses which indicate the pointer "last" position. We
+   ;; need the last coordinate to compute the motion difference.
+   (moving-pointers
+    :initform (make-hash-table :test #'eq)
+    :accessor moving-pointers)
+   ;; The hash table keys are pointers which currently resize the window. Its
+   ;; values are always T.
+   (resize-pointers
+    :initform (make-hash-table :test #'eq)
+    :accessor resize-pointers)
+   ;; Frame is the actual application.
+   (frame
+    :initform nil
+    :initarg :frame
+    :accessor frame)))
+
+(defmethod initialize-instance :after ((object window) &rest args
+                                       &key frame fgc bgc r1 c1 r2 c2)
+  (declare (ignore args))
+  ;; The frame mode is direct, because we are not interested in buffering
+  ;; output that will be buffered (again) in the window.
+  (when frame
+    (setf (frame object)
+          (let ((rows (+ (- r2 r1) 1))
+                (cols (+ (- c2 c1) 1)))
+            (make-instance frame :sink object :mode :dir
+                                 :fgc fgc :bgc bgc
+                                 :r1 2 :c1 3
+                                 :r2 (- rows 1) :c2 (- cols 2))))))
+
+(defmethod handle-repaint ((object window))
+  (flet ((draw-decorations (surface)
+           (let ((rows (l0:rows surface))
+                 (cols (l0:cols surface))
+                 (border-fgc #x8800ff00)
+                 (border-bgc #xffff8800))
+             (block nil
+               (ax:maphash-values (lambda (s)
+                                    (when (eq surface s)
+                                      (setf border-fgc #x8800ff00)
+                                      (setf border-bgc #xff888800)
+                                      (return)))
+                                  (pressed l1:*console*)))
+             (let ((title (title surface)))
+               (l1:ctl (:ink border-fgc border-bgc)
+                       (:clr 1 1 rows cols)
+                       (:ink #x8800ff00 #x22222200)
+                       (:clr 2 3 (- rows 1) (- cols 2))
+                       (:ink border-fgc border-bgc))
+               (l1:out (:row 1 :col 1) title)
+               (l1:out (:row rows :col cols) "/")))))
+    (l1:with-buffer (object)
+      (draw-decorations object)))
+  (handle-repaint (frame object))
+  ;; We need "force" here to reflush the background onto the sink that
+  ;; redraws its own background upon C-r. The more robust solution would be
+  ;; a protocol which allows signaling damaged regions to children. That
+  ;; will be implemented in the l2 module.
+  (l1:flush-output object :force t))
+
+(defmethod l1:handle-event ((object window) (event l0:keyboard-event))
+  (l1:handle-event (frame object) event))
 
 (defmethod l1:handle-event ((object window) (event l0:pointer-event))
   (unless (eq (l0:btn event) :left)
@@ -210,7 +205,7 @@
         (rpt (resize-pointers object)))
     (ecase (l0:state event)
       (:press
-       (case (inside-p object row col t)
+       (case (inside-p object row col)
          (:move   (setf (gethash ptr mpt) (cons row col)))
          (:resize (setf (gethash ptr rpt) t))))
       (:release
@@ -237,6 +232,44 @@
              (setf (car pos) row
                    (cdr pos) col)))))))
 
+
+(defclass peep-frame (l1:surface) ())
+
+(defmethod handle-repaint ((object peep-frame))
+  (l1:with-buffer (object)
+    (draw-peep 1 1 nil)))
+
+
+(defclass text-frame (l1:surface)
+  ((text-buffer
+    :initform "Hello world"
+    :accessor text-buffer)))
+
+(defmethod handle-repaint ((object text-frame)
+                           &aux (text (text-buffer object)))
+  (l1:with-buffer (object)
+    (l1:ctl (:clr 1 1 (l0:rows object) (l0:cols object)))
+    (l1:out (:row 1) text)
+    ;; Our "surface manager" is only capable of "focusing" top-level surfaces,
+    ;; so we check whether the sink (we assume that it is a top-level window)
+    ;; is focused and only then the prompt is printed.
+    (when (eq (l1:sink object) (focused l1:*console*))
+      ;; out should accept now row/col, in that case buffer-cursor position
+      ;; should be left "at the end" of the last output.
+      (multiple-value-bind (mod rem)
+          (truncate (length text) (l0:cols object))
+        (l1:out (:row (+ mod 1) :col (+ rem 1) :txt '(:blink t)) "_")))))
+
+(defmethod l1:handle-event ((object text-frame) (event l0:keyboard-event))
+  (setf (text-buffer object) (format nil "~a" event)))
+
+;; (defclass text-cursor (fm::cursor) ())
+
+;; (defmethod l1:handle-event ((client text-cursor) (event l0:keyboard-event))
+;;   (fm::key-case event
+;;     (:backsp (decf ))))
+
+
 (defun start-surface-manager ()
   (l1:with-console (:ios *terminal-io* :console-class 'surface-manager)
     (loop with console = l1:*console*
