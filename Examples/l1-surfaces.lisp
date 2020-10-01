57423950790fafe42ba78e04d39fc637b1a8639e
Daniel Kochmański <daniel@turtleware.eu>
2020-10-01 09:16:17 +0200

    example: surfaces: implement pointer gestures for windows

--- Examples/l1-surfaces.lisp
+++ Examples/l1-surfaces.lisp
@@ -5,20 +5,39 @@
 ;;; Features:
 ;;;
 ;;; - a custom implementation of the console
-;;; - two moveable and resizable surfaces
-;;; - keyboard events are redirected to a focused surface (if any)
-;;; - pointer events are redirected to a surface below the cursor
+;;; - multiple moveable and resizable surfaces
+;;; - events are redirected to a focused/pressed surface
+;;; - multiple pointers may be used simultaneously
 
 (defclass window (l1:surface)
   ((title
     :initarg :title
-    :accessor title)))
+    :accessor title)
+   ;; The hash table keys are pointers which are currently moving the window
+   ;; and its values are conses which indicate the pointer "last" position. We
+   ;; need the last coordinate to compute the motion difference.
+   (moving-pointers
+    :initform (make-hash-table :test #'eq)
+    :accessor moving-pointers)
+   ;; The hash table keys are pointers which currently resize the window. Its
+   ;; values are always T.
+   (resize-pointers
+    :initform (make-hash-table :test #'eq)
+    :accessor resize-pointers)))
 
 (defclass surface-manager (l1:console)
   ((surfaces
     :accessor surfaces)
+   ;; When a surface is "focused" all keyboard events are delivered to it.
    (focused
-    :accessor focused))
+    :initform nil
+    :accessor focused)
+   ;; When a surface is "pressed" all pointer events are delivered to it.
+   ;; Multiple surfaces may be pressed by different pointers. Pointer is a key
+   ;; and the surface is a value.
+   (pressed
+    :initform (make-hash-table)
+    :accessor pressed))
   (:default-initargs
    :fgc +white+
    :bgc +black+))
@@ -37,28 +56,42 @@
                              :sink object
                              :fgc +black+
                              :bgc +grey2+
-                             :r1 13 :c1 10 :r2 19 :c2 23)))
-  (setf (focused object) nil))
+                             :r1 13 :c1 10 :r2 19 :c2 23))))
 
 (defun draw-decorations (surface)
   (multiple-value-bind (r1 c1 r2 c2) (l1:bbox surface)
     (l1:with-buffer ((l1:sink surface))
-      (l1:ctl (:ink #x0000ff00 #xffff8800))
+      (block nil
+        (ax:maphash-values (lambda (s)
+                             (when (eq surface s)
+                               (l1:ctl (:ink #x8800ff00 #xff888800))
+                               (return)))
+                           (pressed l1:*console*))
+        (l1:ctl (:ink #x8800ff00 #xffff8800)))
       (let* ((length (+ (- c2 c1) 5))
-             (hbar (make-string length :initial-element #\space)))
+             (hbar (make-string length :initial-element #\space))
+             (title (title surface)))
         (l1:out (:row (1- r1) :col (- c1 2)) hbar)
-        (draw-text (title surface) (1- r1) (floor (+ c2 c1 1) 2) :align :center)
+        (l1:out (:row (1- r1) :col (- c1 2)) title)
+        (when (eq surface (focused l1:*console*))
+          (l1:out (:row (1- r1)
+                   :col (+ (- c1 2) (length title))
+                   :txt '(:blink t)) "█"))
         (l1:out (:row (1+ r2) :col (- c1 2)) hbar)
-        (l1:out (:row (1+ r2) :col (1+ c2)) "30"))
+        (l1:out (:row (1+ r2) :col (+ c2 2)) "/"))
       (loop for r from r1 upto r2
-            do (l1:out (:row r :col (- c1 2) :bgc #xffff8800) "  ")
-               (l1:out (:row r :col (+ c2 1) :bgc #xffff8800) "  ")))))
+            do (l1:out (:row r :col (- c1 2)) "  ")
+               (l1:out (:row r :col (+ c2 1)) "  ")))))
 
+;;; A function of the same name (and similar purpose) will be introduced for
+;;; display lists in the l2 module.
 (defgeneric handle-repaint (object)
   (:method (object) nil))
 
 (defmethod handle-repaint ((object surface-manager))
-  (mapc #'handle-repaint (surfaces object))
+  (l1:ctl (:ink #x00000000 #x00000000)
+          (:clr 1 1 (l0:rows object) (l0:cols object)))
+  (mapc #'handle-repaint (reverse (surfaces object)))
   (l1:flush-output object))
 
 (defmethod handle-repaint ((object window))
@@ -70,6 +103,105 @@
     ;; will be implemented in the l2 module.
     (l1:flush-output object :force t))
   (draw-decorations object))
+
+(defun inside-p (surface row col &optional decorations)
+  (multiple-value-bind (r1 c1 r2 c2)
+      (l1:bbox surface)
+    ;; +/- 1/2 is for decorations.
+    (cond ((and (<= r1 row r2)
+                (<= c1 col c2))
+           :surface)
+          ((null decorations)
+           nil)
+          ((and (= row (+ r2 1))
+                (= col (+ c2 2)))
+           :resize)
+          ((and (<= (- r1 1) row (+ r2 1))
+                (<= (- c1 2) col (+ c2 2)))
+           :move)
+          (t
+           nil))))
+
+;;; Our event handling is quite simplistic. The pointer events left button
+;;; press and left button release are used to set the pressed window, and the
+;;; pointer event left button press is used to set the focused window. When
+;;; they are made above an empty area, then nil is assigned to the appropriate
+;;; field. When a window is pressed, then all pointer events are redirected to
+;;; that window. When a window is focused, then all keyboard events are
+;;; redirected to that window.
+(defmethod l1:handle-event ((object surface-manager) (event l0:pointer-event)
+                            &aux
+                              (ptr (fm::pointer event))
+                              (state (l0:state event))
+                              (button (l0:btn event)))
+  (when (and (eq button :left)
+             (eq state :release))
+    (ax:when-let ((pressed (gethash ptr (pressed object))))
+      (l1:handle-event pressed event)
+      (remhash ptr (pressed object)))
+    (return-from l1:handle-event))
+  (ax:if-let ((surface (gethash ptr (pressed object))))
+    ;; Event coordinates are specified in the console coordinate system.
+    (l1:handle-event surface event)
+    ;; If we end up in the "else" branch, then no surface is pressed. Our
+    ;; handling is quite simplistic for that it only distributes events and
+    ;; manages focus.
+    (when (and (eq button :left)
+               (eq state :press))
+      (let ((row (l0:row event))
+            (col (l0:col event)))
+        (ax:if-let ((sur (loop for surface in (surfaces object)
+                               when (inside-p surface row col t)
+                                 do (return surface))))
+          (progn
+            (setf (gethash ptr (pressed object)) sur)
+            (setf (focused object) sur)
+            ;; cycle to the top
+            (let ((surfaces (surfaces object)))
+              (unless (eq sur (first surfaces))
+                (setf (surfaces object)
+                      (list* sur (remove sur surfaces)))))
+            (l1:handle-event sur event))
+          ;; The button was pressed over an empty area.
+          (remhash ptr (pressed object)))))))
+
+(defmethod l1:handle-event ((object surface-manager) (event l0:keyboard-event))
+  ;; We want to retain global console gestures for keyboard (C-r etc).
+  (call-next-method)
+  (ax:when-let ((surface (focused object)))
+    (l1:handle-event surface event)))
+
+(defmethod l1:handle-event ((object window) (event l0:keyboard-event)))
+
+(defmethod l1:handle-event ((object window) (event l0:pointer-event))
+  (unless (eq (l0:btn event) :left)
+    (return-from l1:handle-event))
+  (let ((ptr (fm::pointer event))
+        (row (l0:row event))
+        (col (l0:col event)))
+    (ecase (l0:state event)
+      (:press
+       (case (inside-p object row col t)
+         (:move   (setf (gethash ptr (moving-pointers object))
+                        (cons row col)))
+         (:resize (setf (gethash ptr (resize-pointers object))
+                        t))))
+      (:release
+       (remhash ptr (moving-pointers object))
+       (remhash ptr (resize-pointers object)))
+      (:motion
+       (if (gethash ptr (resize-pointers object))
+           (fm::reshape-buffer object
+                               (fm::r1 object)
+                               (fm::c1 object)
+                               (- row 1)
+                               (- col 2))
+           (ax:when-let ((pos (gethash ptr (moving-pointers object))))
+             (fm::move-buffer object
+                              (- row (car pos))
+                              (- col (cdr pos)))
+             (setf (car pos) row
+                   (cdr pos) col)))))))
 
 (defun start-surface-manager ()
   (l1:with-console (:ios *terminal-io* :console-class 'surface-manager)
